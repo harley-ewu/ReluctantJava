3b8be0fa64e8835bad7485a9a158e1e95dd35797
DennysGuy <mattmatriciano@gmail.com>
2023-10-05 18:34:55 -0700

    added a source and tests folder. dropped the two existing packages into source.

---
+++ src/Attributes/Attribute.java
@@ -0,0 +1,34 @@
+package Attributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Attribute {
+    // List of attributes for the class.
+    private List<Attribute> attributes = new ArrayList<>();
+    // Attribute name and variable type.
+    private String name;
+    private String type;
+
+    public Attribute(final String name, final String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public void addAttribute(final String name, final String type) {
+        // Initializing a new attribute and adding it to the list.
+        Attribute newAttribute = new Attribute(name, type);
+        attributes.add(newAttribute);
+    }
+
+    public void deleteAttribute(final String name) {
+        // Loop to check each attribute in the list.
+        for (Attribute attribute : attributes) {
+            // Checking if the name of the attribute to be deleted matches with the attribute that was found.
+            if (attribute.name.equals(name)) {
+                // Removing the attribute and leaving the loop.
+                attributes.remove(attribute);
+                break;
+            }
+        }
+    }
+}
