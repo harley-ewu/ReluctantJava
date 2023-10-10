12c89a9629d0cbcb8ee9c41d1772100b98c3fa12
Nolan Posey <nposey1@ewu.edu>
2023-10-10 14:18:05 -0700

    Added integration test for Save and Load methods.

--- src/SaveLoadSystem/SaveLoadSystemTest.java
+++ src/SaveLoadSystem/SaveLoadSystemTest.java
@@ -20,15 +20,48 @@ public class SaveLoadSystemTest {
         String home = System.getProperty("user.home");
 
         list.add(testClass);
-        saveLoad.defaultPathSave("SaveLoadTestFile", list);
-        String path = String.valueOf(Paths.get(home).resolve("SaveLoadTestFile.json"));
+        saveLoad.defaultPathSave("SaveTestFile", list);
+        String path = String.valueOf(Paths.get(home).resolve("SaveTestFile.json"));
         File file = new File(path);
 
         // I assert that the file created is not empty
         Assert.assertTrue(file.length() > 0);
     }
 
+    @Test
     protected void testLoadFromDefaultPath() {
 
+    }
+
+    @Test
+    protected void testIntegrationOfSaveAndLoad() {
+
+        /**
+         * Initialize the test class, the ArrayList to store the test class,
+         * the save/load class, and get the path to the default directory.
+         */
+        MockUmlClass testClass = new MockUmlClass("test", "This is a test");
+        ArrayList<MockUmlClass> list = new ArrayList<>();
+        SaveLoadSystem saveLoad = new SaveLoadSystem();
+        String defaultPath = System.getProperty("user.home");
+
+        /**
+         * Add the test class to the ArrayList, Save the ArrayList to a JSON file named SaveLoadIntegrationTest.json
+         * in the default directory, then get the file path to the created file.
+         */
+        list.add(testClass);
+        saveLoad.defaultPathSave("SaveLoadIntegrationTest", list);
+        String path = String.valueOf(Paths.get(defaultPath).resolve("SaveLoadIntegrationTest.json"));
+
+        /**
+         * Load the file into the ArrayList.
+         */
+        list = saveLoad.load(path);
+
+        // I assert that the description of the saved class is the same as the loaded class.
+        assertTrue(testClass.getDescription().equals(list.getFirst().getDescription()));
+
+        // I assert that the name of the saved class is the same as the loaded class.
+        assertTrue(testClass.getName().equals(list.getFirst().getName()));
     }
 }
