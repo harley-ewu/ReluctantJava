129cf9e4d8c42ad359daae945bbd4fe3c18d53bc
Tanner Stephenson <tstephenson2@ewu.edu>
2023-10-13 11:46:29 -0700

    Merge conflit resolution

--- src/Attributes/Attribute.java
+++ src/Attributes/Attribute.java
@@ -2,6 +2,7 @@ package Attributes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.jar.Attributes;
 
 public class Attribute {
     // List of attributes for the class.
