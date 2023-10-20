66112a5a74ce1d7b06a5016438b12c4402dea2b4
Nolan Posey <nposey1@ewu.edu>
2023-10-19 17:50:41 -0700

    Added the SaveLoadSystem back into the CLI class

--- src/CLI/CommandLineInterface.java
+++ src/CLI/CommandLineInterface.java
@@ -1,6 +1,7 @@
 package CLI;
 
 import Diagram.Diagram;
+import SaveLoadSystem.SaveLoadSystem;
 //import SaveLoadSystem.SaveLoadSystem;
 
 import java.nio.file.InvalidPathException;
@@ -38,7 +39,7 @@ public class CommandLineInterface {
                 case 1 -> currentDiagram = createNewDiagram(currentDiagram);
                 case 2 -> viewDiagram(currentDiagram);
                 case 3 -> editDiagram(currentDiagram);
-                //case 4 -> saveDiagram(currentDiagram);
+                case 4 -> saveDiagram(currentDiagram);
                 case 5 -> currentDiagram = loadDiagram();
                 case 6 -> help();
                 case 7 -> shouldTerminate = exit(currentDiagram);
@@ -118,7 +119,7 @@ public class CommandLineInterface {
         return userInput >= 1 && userInput <= MAX_CHOICES;
     }
 
-    /*private static void savePrompt(Diagram diagram) {
+    private static void savePrompt(Diagram diagram) {
         char userChoice;
         Scanner scan = new Scanner(System.in);
 
@@ -136,14 +137,14 @@ public class CommandLineInterface {
             case '2' -> saveToCustomPath(diagram);
             default -> System.out.println("Continuing without saving.");
         }
-    }*/
+    }
 
-    /*private static void saveToDefaultPath(Diagram diagram) {
-        SaveLoadSystem.saveDefault("", diagram.getClassList());
+    private static void saveToDefaultPath(Diagram diagram) {
+        SaveLoadSystem.saveDefaultCLI(diagram.getTitle(), diagram);
         System.out.println("Saved to the default path.");
-    }*/
+    }
 
-    /*private static void saveToCustomPath(Diagram diagram) {
+    private static void saveToCustomPath(Diagram diagram) {
         Scanner scan = new Scanner(System.in);
         boolean saveSuccessful = false;
 
@@ -153,7 +154,7 @@ public class CommandLineInterface {
 
             try {
                 Path filePath = Paths.get(customPath);
-                SaveLoadSystem.saveCustom(filePath.toString(), "", diagram.getClassList());
+                SaveLoadSystem.saveCustomCLI(filePath.toString(), diagram.getTitle(), diagram);
                 System.out.println("Saved to custom path: " + filePath);
                 saveSuccessful = true;
             } catch (InvalidPathException e) {
@@ -182,14 +183,14 @@ public class CommandLineInterface {
                 }
             }
         }
-    }*/
+    }
 
 
 
     private static Diagram createNewDiagram(Diagram currentDiagram){
-        /*if(currentDiagram != null){
+        if(currentDiagram != null){
             savePrompt(currentDiagram);
-        }*/
+        }
 
         String diagramName = getDiagramName();
         return new Diagram(diagramName);
@@ -210,16 +211,22 @@ public class CommandLineInterface {
 
         currentDiagram.menu();
     }
-    /*private static void saveDiagram(Diagram currentDiagram){
+    private static void saveDiagram(Diagram currentDiagram){
         savePrompt(currentDiagram);
-    }*/
+    }
     private static Diagram loadDiagram() {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter the path to the file you want to load:");
-
+        System.out.print("--> ");
         String filePath = scan.nextLine();
 
+        System.out.println("Enter the name of the file you want to load:");
+        System.out.print("--> ");
+        String fileName = scan.nextLine();
+
+        Diagram diagram = SaveLoadSystem.loadDiagramCLI(filePath, fileName);
+        /*
         try {
             //var loadedClasses = SaveLoadSystem.load(filePath);
             System.out.println("Diagram loaded successfully.");
@@ -230,9 +237,9 @@ public class CommandLineInterface {
             System.out.println("Invalid file path. Please enter a valid path.");
         } catch (Exception e) {
             System.out.println("An error occurred while loading the diagram.");
-        }
+        }*/
 
-        return null;
+        return diagram;
     }
 
     /*
@@ -267,7 +274,7 @@ public class CommandLineInterface {
     * */
 
     private static boolean exit(Diagram currentDiagram) {
-        //savePrompt(currentDiagram);
+        savePrompt(currentDiagram);
         return true;
     }
 }
