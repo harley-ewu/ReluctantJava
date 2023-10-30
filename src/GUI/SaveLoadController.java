b0c854cb560f7c493b1cde07c25be661e4a0b6a0
Nolan Posey <nposey1@ewu.edu>
2023-10-30 15:52:44 -0700

    Made skeleton of SaveLoadController.java and its methods.

---
+++ src/GUI/SaveLoadController.java
@@ -0,0 +1,18 @@
+package GUI;
+
+import Diagram.Diagram;
+
+public class SaveLoadController {
+
+    public void SaveGUIHandled(){
+
+    }
+
+    public void SaveAsGUIHandled(){
+
+    }
+
+    public Diagram LoadGUIHandled(){
+        return null;
+    }
+}
