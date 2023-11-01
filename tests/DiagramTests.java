e1249d72070d39646a3c900e4abb46e342560d82
Nick Parkman <nparkman@ewu.edu>
2023-11-01 15:36:24 -0700

    listAllRelationships and listOneClassRelationships tests and update

--- tests/DiagramTests.java
+++ tests/DiagramTests.java
@@ -102,5 +102,74 @@ public class DiagramTests {
 		assertEquals(testRelationship, UMLDiagram.findSingleRelationship(testClass, testClass2));
 		assertEquals(testRelationship, UMLDiagram.findSingleRelationship(testClass2, testClass));
 	}
-	
+
+	@Test
+	void listAllRelationshipsTest() throws Exception {
+		Diagram UMLDiagram = new Diagram("");
+
+		Class testClass = new Class("testClass");
+		UMLDiagram.addClass(testClass.getClassName());
+		Class testClass2 = new Class("testClass2");
+		UMLDiagram.addClass(testClass2.getClassName());
+		Class testClass3 = new Class("testClass3");
+		UMLDiagram.addClass(testClass3.getClassName());
+
+		Relationship testRelationship = new Relationship(Relationship.RelationshipType.Aggregation, testClass, testClass2, 1, 1, true);
+		UMLDiagram.addRelationship(testRelationship);
+
+		Relationship testRelationship2 = new Relationship(Relationship.RelationshipType.Aggregation, testClass, testClass3, 1, 1, true);
+		UMLDiagram.addRelationship(testRelationship2);
+
+		Relationship testRelationship3 = new Relationship(Relationship.RelationshipType.Aggregation, testClass2, testClass3, 1, 1, true);
+		UMLDiagram.addRelationship(testRelationship3);
+
+		String testStr = "Relationship List: \n"
+				+ "1: testClass2 has a Aggregation relationship with testClass3\n"
+				+ "Owner: true\n"
+				+ "testClass2 Class Cardinality: 1\n"
+				+ "testClass3 Class Cardinality: 1\n"
+				+ "2: testClass has a Aggregation relationship with testClass2\n"
+				+ "Owner: true\n"
+				+ "testClass Class Cardinality: 1\n"
+				+ "testClass2 Class Cardinality: 1\n"
+				+ "3: testClass has a Aggregation relationship with testClass3\n"
+				+ "Owner: true\n"
+				+ "testClass Class Cardinality: 1\n"
+				+ "testClass3 Class Cardinality: 1\n";
+
+		assertEquals(testStr, UMLDiagram.listAllRelationships());
+	}
+
+	@Test
+	void listOneClassRelationshipsTest() {
+		Diagram UMLDiagram = new Diagram("");
+
+		Class testClass = new Class("testClass");
+		UMLDiagram.addClass(testClass.getClassName());
+		Class testClass2 = new Class("testClass2");
+		UMLDiagram.addClass(testClass2.getClassName());
+		Class testClass3 = new Class("testClass3");
+		UMLDiagram.addClass(testClass3.getClassName());
+
+		Relationship testRelationship = new Relationship(Relationship.RelationshipType.Aggregation, testClass, testClass2, 1, 1, true);
+		UMLDiagram.addRelationship(testRelationship);
+
+		Relationship testRelationship2 = new Relationship(Relationship.RelationshipType.Aggregation, testClass, testClass3, 1, 1, true);
+		UMLDiagram.addRelationship(testRelationship2);
+
+		Relationship testRelationship3 = new Relationship(Relationship.RelationshipType.Aggregation, testClass2, testClass3, 1, 1, true);
+		UMLDiagram.addRelationship(testRelationship3);
+
+		String testStr = "Relationships: \n"
+				+ "1: testClass has a Aggregation relationship with testClass3\n"
+				+ "Owner: true\n"
+				+ "testClass Class Cardinality: 1\n"
+				+ "testClass3 Class Cardinality: 1\n"
+				+ "2: testClass has a Aggregation relationship with testClass2\n"
+				+ "Owner: true\n"
+				+ "testClass Class Cardinality: 1\n"
+				+ "testClass2 Class Cardinality: 1\n";
+
+		assertEquals(testStr, UMLDiagram.listOneClassRelationships(testClass));
+	}
 }
