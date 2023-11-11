26e7c2d5dea923b7ea0dc7881438ebc4c5fb5c98
Nolan Posey <nposey1@ewu.edu>
2023-11-11 13:59:37 -0800

    Added documentation to Class, Method, and field classes.

--- src/Attributes/Field.java
+++ src/Attributes/Field.java
@@ -16,22 +16,45 @@ public class Field{
         this.primitive = primitive;
     }
 
+    /**
+     * Description: Gets the name of the primitive type stored in the field.
+     * @return - Returns a string with the type of the primitive.
+     */
     public String getPrimitive() {
         return this.primitive.replaceAll("[\\[\\]]", "");
     }
 
+    /**
+     * Description: Sets the primitive type for a field.
+     * @param primitive - The name of the primitive for the field.
+     */
     public void setPrimitive(String primitive) {
         this.primitive = primitive;
     }
 
+    /**
+     * Description: Gets the name of the field.
+     * @return - Returns a string with the name of the field.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Description: Sets the name of the field.
+     * @param name - The new name for the field.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Description: Compares an object to a field to determine if they have the same information.
+     * @param o - The object being compared to the field.
+     * @return - Returns true if they're the same exact object or if they share the same
+     * name and primitive name, returns false if the object is null or not of type field, also returns false
+     * if either their names are not the same, their primitives are not the same, or both.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -40,6 +63,10 @@ public class Field{
         return this.primitive.equals(field.primitive) && this.name.equals(field.name);
     }
 
+    /**
+     * Description: Converts the information in the field object into a string.
+     * @return - Returns a string with the name and primitive name of the field object.
+     */
     @Override
     public String toString() {
         return ( this.name + ": " + this.primitive.replaceAll("[\\[\\]]", ""));
--- src/Attributes/Method.java
+++ src/Attributes/Method.java
@@ -33,22 +33,42 @@ public class Method{
         this.parameters.addAll(parameter);
     }
 
+    /**
+     * Description: Gets the list of params inside the method object.
+     * @return - Returns an ArrayList of type string with all the params.
+     */
     public ArrayList<String> getParameters() {
         return parameters;
     }
 
+    /**
+     * Description: Gets the name of the method object.
+     * @return - Returns a string with the name of the method object.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Description: Sets the name of the method object to a new name.
+     * @param name - the new name of the method object.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Description: Adds a param to the list of params for the method object.
+     * @param parameter - the name of the new param
+     */
     public void addParameter(String parameter) {
         this.parameters.add(parameter);
     }
 
+    /**
+     * Description: Removes a param from the list of params in the method object.
+     * @param input - The index of the param to be removed.
+     */
     public void removeParameter(int input) {
         if (input < 1 || input > this.parameters.size()+1) {
             return;
@@ -56,19 +76,28 @@ public class Method{
         this.parameters.remove(input-1);
     }
 
-
-    @Override
-    public String toString() {
-        return (this.name + "(" + parameters.toString().replaceAll("[\\[\\]]", "") + ")");
-    }
-
-
-
+    /**
+     * Description: Compares and object to the calling method object to see if they have the same information.
+     * @param o - The object being compared to the method object.
+     * @return - Returns true the object being compared is the same exact method object or if
+     * the params are the same and the name of the methods are the same, returns false if the
+     * object being compared is null or not an instance of method, also returns false if the
+     * params are not the same, the name is not the same, or both.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Method method = (Method) o;
         return this.parameters.equals(method.parameters) && this.name.equals(method.name);
+    }
+
+    /**
+     * Description: Converts the information stored inside the method object into a string.
+     * @return - Returns a string of the name and params of the method object.
+     */
+    @Override
+    public String toString() {
+        return (this.name + "(" + parameters.toString().replaceAll("[\\[\\]]", "") + ")");
     }
 }
