9704551989fb7ed8d32accf3a6d91588296518a0
Tanner Stephenson <tstephenson2@ewu.edu>
2023-11-15 15:48:54 -0800

    Start of memento

---
+++ src/Diagram/DiagramMemento.java
@@ -0,0 +1,33 @@
+package Diagram;
+
+import Relationships.Relationship;
+import Class.Class;
+
+import java.util.HashMap;
+
+public class DiagramMemento {
+
+    private Diagram diagram;
+    private String saveLocation = null;
+    private String title;
+    private HashMap<String, Class> classList;
+    private HashMap<String, Relationship> relationshipList;
+
+    public DiagramMemento(Diagram diagram, final String title) {
+
+        if (title == null) {
+            throw new NullPointerException("invalid param in Diagram constructor");
+        }
+        this.diagram = diagram;
+        this.title = title;
+        this.classList = new HashMap<>();
+        this.relationshipList = new HashMap<>();
+    }
+
+    public void restore() {
+        this.diagram.setTitle(this.title);
+        this.diagram.setSaveLocation(saveLocation);
+        this.diagram.setClassList(this.classList);
+        this.diagram.setRelationshipList(relationshipList);
+    }
+}
