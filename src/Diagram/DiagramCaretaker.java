8357adbf78c6cefd20aa96226e7e3450bb8337b9
TannerStephenson <117472530+TannerStephenson@users.noreply.github.com>
2023-11-22 21:35:19 -0800

    Duplicate if checks

--- src/Diagram/DiagramCaretaker.java
+++ src/Diagram/DiagramCaretaker.java
@@ -9,18 +9,19 @@ public class DiagramCaretaker {
     private int currentIndex = -1;
 
     public void undo(Diagram diagram) {
-        if(this.currentIndex == this.diagramMementoList.size() -1){
+        DiagramMemento duplicateCheck = new DiagramMemento(diagram);
+        if(this.currentIndex == this.diagramMementoList.size() -1 && !this.diagramMementoList.contains(duplicateCheck)){
             diagram.createSnapshot();
         }
-        if (this.getCurrentIndex() != -1) {
-            DiagramMemento memento = this.getDiagram(this.getCurrentIndex());
+        if (this.getCurrentIndex() != 0) {
+            DiagramMemento memento = this.getDiagram(this.getCurrentIndex() - 1);
             diagram.applyMemento(memento);
             this.decrementIndex();
         }
     }
 
     public void redo(Diagram diagram) {
-        if (this.getCurrentIndex() < this.getDiagramMementoList().size()) {
+        if (this.getCurrentIndex() < this.getDiagramMementoList().size() - 1) {
             this.incrementIndex();
             DiagramMemento memento = this.getDiagram(this.getCurrentIndex());
             diagram.applyMemento(memento);
