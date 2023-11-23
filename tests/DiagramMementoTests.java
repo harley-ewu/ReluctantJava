d125975a6dba02e50074b26588b8ac0db7b61d5b
TannerStephenson <117472530+TannerStephenson@users.noreply.github.com>
2023-11-22 22:08:15 -0800

    Current implementation of undo and redo.

--- tests/DiagramMementoTests.java
+++ tests/DiagramMementoTests.java
@@ -20,110 +20,144 @@ public class DiagramMementoTests {
     }
 
     @Test
-    public void undoDiagramNameTest() {
-        diagram.setTitle("NewDiagram");
-        assertEquals("NewDiagram", diagram.getTitle());
-
-        diagram.undo();
-        assertEquals("OriginalDiagram", diagram.getTitle());
-    }
-
-    @Test
-    public void redoDiagramNameTest() {
-        diagram.setTitle("NewDiagram");
-        assertEquals("NewDiagram", diagram.getTitle());
-
-        diagram.undo();
-        assertEquals("OriginalDiagram", diagram.getTitle());
-
-        diagram.redo();
-        assertEquals("NewDiagram", diagram.getTitle());
-    }
-
-    @Test
     public void undoRedoUndoTest() {
-        diagram.setTitle("NewDiagram");
-        assertEquals("NewDiagram", diagram.getTitle());
+        diagram.addClass("TestClass");
+        diagram.addClass("AnotherClass");
+        String expected = "\n" +
+                "--------------------------\n" +
+                "Diagram: OriginalDiagram\n" +
+                "\n" +
+                "\n" +
+                "--------------------------\n" +
+                "Class Name: AnotherClass\n" +
+                "--------------------------\n" +
+                "Fields: \n" +
+                "Methods: \n" +
+                "\n" +
+                "--------------------------\n" +
+                "\n" +
+                "--------------------------\n" +
+                "Class Name: TestClass\n" +
+                "--------------------------\n" +
+                "Fields: \n" +
+                "Methods: \n" +
+                "\n" +
+                "--------------------------\n" +
+                "\n" +
+                "\n" +
+                "--------------------------\n" +
+                "Relationship List: \n" +
+                "\n" +
+                "\n" +
+                "--------------------------\n";
+        assertEquals(expected, diagram.toString());
 
+        String newExpectation =
+                "\n" +
+                        "--------------------------\n" +
+                        "Diagram: OriginalDiagram\n" +
+                        "\n" +
+                        "\n" +
+                        "--------------------------\n" +
+                        "Class Name: TestClass\n" +
+                        "--------------------------\n" +
+                        "Fields: \n" +
+                        "Methods: \n" +
+                        "\n" +
+                        "--------------------------\n" +
+                        "\n" +
+                        "\n" +
+                        "--------------------------\n" +
+                        "Relationship List: \n" +
+                        "\n" +
+                        "\n" +
+                        "--------------------------\n";
         diagram.undo();
-        assertEquals("OriginalDiagram", diagram.getTitle());
-
+        assertEquals(newExpectation, diagram.toString());
         diagram.redo();
-        assertEquals("NewDiagram", diagram.getTitle());
-
-        diagram.undo();
-        assertEquals("OriginalDiagram", diagram.getTitle());
+        assertEquals(expected, diagram.toString());
     }
 
     @Test
     public void consecutiveUndos() {
-        diagram.setTitle("OneFish");
-        assertEquals("OneFish", diagram.getTitle());
-        diagram.setTitle("TwoFish");
-        assertEquals("TwoFish", diagram.getTitle());
-        diagram.setTitle("RedFish");
-        assertEquals("RedFish", diagram.getTitle());
-        diagram.setTitle("BlueFish");
-        assertEquals("BlueFish", diagram.getTitle());
-
+        diagram.addClass("TestClass");
+        diagram.addClass("AnotherClass");
+        diagram.addClass("a");
+        diagram.addClass("b");
+        diagram.addClass("c");
         diagram.undo();
-        assertEquals("RedFish", diagram.getTitle());
-
         diagram.undo();
-        assertEquals("TwoFish", diagram.getTitle());
-
         diagram.undo();
-        assertEquals("OneFish", diagram.getTitle());
-
-        diagram.undo();
-        assertEquals("OriginalDiagram", diagram.getTitle());
-
-        diagram.undo();
-        assertEquals("OriginalDiagram", diagram.getTitle());
+        String expected = "\n" +
+                "--------------------------\n" +
+                "Diagram: OriginalDiagram\n" +
+                "\n" +
+                "\n" +
+                "--------------------------\n" +
+                "Class Name: AnotherClass\n" +
+                "--------------------------\n" +
+                "Fields: \n" +
+                "Methods: \n" +
+                "\n" +
+                "--------------------------\n" +
+                "\n" +
+                "--------------------------\n" +
+                "Class Name: TestClass\n" +
+                "--------------------------\n" +
+                "Fields: \n" +
+                "Methods: \n" +
+                "\n" +
+                "--------------------------\n" +
+                "\n" +
+                "\n" +
+                "--------------------------\n" +
+                "Relationship List: \n" +
+                "\n" +
+                "\n" +
+                "--------------------------\n";
+        assertEquals(expected, diagram.toString());
     }
 
     @Test
     public void consecutiveRedos() {
-        diagram.setTitle("OneFish");
-        assertEquals("OneFish", diagram.getTitle());
-        diagram.setTitle("TwoFish");
-        assertEquals("TwoFish", diagram.getTitle());
-        diagram.setTitle("RedFish");
-        assertEquals("RedFish", diagram.getTitle());
-        diagram.setTitle("BlueFish");
-        assertEquals("BlueFish", diagram.getTitle());
-        diagram.setTitle("Finished");
-        assertEquals("Finished", diagram.getTitle());
-
+        diagram.addClass("TestClass");
+        diagram.addClass("AnotherClass");
+        String expected = "\n" +
+                "--------------------------\n" +
+                "Diagram: OriginalDiagram\n" +
+                "\n" +
+                "\n" +
+                "--------------------------\n" +
+                "Class Name: AnotherClass\n" +
+                "--------------------------\n" +
+                "Fields: \n" +
+                "Methods: \n" +
+                "\n" +
+                "--------------------------\n" +
+                "\n" +
+                "--------------------------\n" +
+                "Class Name: TestClass\n" +
+                "--------------------------\n" +
+                "Fields: \n" +
+                "Methods: \n" +
+                "\n" +
+                "--------------------------\n" +
+                "\n" +
+                "\n" +
+                "--------------------------\n" +
+                "Relationship List: \n" +
+                "\n" +
+                "\n" +
+                "--------------------------\n";
+        assertEquals(expected, diagram.toString());
         diagram.undo();
-        assertEquals("BlueFish", diagram.getTitle());
-
         diagram.undo();
-        assertEquals("RedFish", diagram.getTitle());
-
         diagram.undo();
-        assertEquals("TwoFish", diagram.getTitle());
-
-        diagram.undo();
-        assertEquals("OneFish", diagram.getTitle());
-
-        diagram.undo();
-        assertEquals("OriginalDiagram", diagram.getTitle());
-
         diagram.redo();
-        assertEquals("OneFish", diagram.getTitle());
-
         diagram.redo();
-        assertEquals("TwoFish", diagram.getTitle());
-
         diagram.redo();
-        assertEquals("RedFish", diagram.getTitle());
-
         diagram.redo();
-        assertEquals("BlueFish", diagram.getTitle());
-
-        diagram.redo();
-        assertEquals("Finished", diagram.getTitle());
+        assertEquals(expected, diagram.toString());
     }
 
     @Test
